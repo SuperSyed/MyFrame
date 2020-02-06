a86f899602fb84c9f6dc896a4340af4d773107b6
yollinfo <info@yoll.io>
2020-02-06 18:09:24 -0500

    initial

--- src/test/java/utilities/Helper.java
+++ src/test/java/utilities/Helper.java
@@ -14,7 +14,7 @@ import java.util.Random;
 import static org.junit.Assert.assertTrue;
 
 
-public class BrowserUtils {
+public class Helper {
 
     public static void wait(int secs) {
         try {
